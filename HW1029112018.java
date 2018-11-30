92bd3978a026157d29ca097867b5bf7e3f099b51
Hidden <44298004+archAng30@users.noreply.github.com>
2018-11-30 18:35:35 +0200

    Create HW1029112018.java

---
+++ HW1029112018.java
@@ -0,0 +1,17 @@
+package HW1029112018;
+
+public class HW1029112018 
+{
+    public static void main(String[] args) 
+    {
+        int i = 0;
+        
+        for(i = -10; i < 11; i++)
+        {
+            if(!((i % 2) == 0))
+            {
+                System.out.println(i);
+            }
+        }
+    }
+}
