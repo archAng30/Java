3814561b60dc640c19136fd9d8744c63dcc7ff3a
Hidden <44298004+archAng30@users.noreply.github.com>
2018-11-30 18:30:34 +0200

    Create HW9029112018.java

---
+++ HW9029112018.java
@@ -0,0 +1,14 @@
+package HW9029112018;
+
+public class HW9029112018 
+{
+    public static void main(String[] args) 
+    {
+        int i = 0;
+        
+        for(i = -20; i < 51; i++)
+        {
+            System.out.println(i);
+        }
+    }
+}
