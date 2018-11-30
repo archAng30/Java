1e743b92294e95befaa411aecd8bff2c6520517c
Hidden <44298004+archAng30@users.noreply.github.com>
2018-11-30 18:27:07 +0200

    Create HW8028112018.java

---
+++ HW8028112018.java
@@ -0,0 +1,14 @@
+package HW8028112018;
+
+public class HW8028112018 
+{
+    public static void main(String[] args) 
+    {
+        int i = 0;
+        
+        for(i = 1; i < 101; i++)
+        {
+            System.out.println(i);
+        }
+    }
+}
