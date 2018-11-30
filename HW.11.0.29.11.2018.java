c5e67e0ce3a23832d6831773235034eac53bf039
Hidden <44298004+archAng30@users.noreply.github.com>
2018-11-30 21:54:13 +0200

    Create HW.11.0.29.11.2018.java

---
+++ HW.11.0.29.11.2018.java
@@ -0,0 +1,14 @@
+package HW11029112018;
+
+public class HW11029112018 
+{
+    public static void main(String[] args) 
+    {
+        int i = 0;
+        
+        for(i = 10; i > 0; i--)
+        {
+            System.out.println(i);
+        }
+    }
+}
